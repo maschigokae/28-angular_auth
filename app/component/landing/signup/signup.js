48953d7536ec92de534ff46da6f139d89e130314
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-30 22:26:48 -0800

    build out controller for signup component

--- app/component/landing/signup/signup.js
+++ app/component/landing/signup/signup.js
@@ -1,1 +1,25 @@
 'use strict';
+
+module.exports = {
+  template: require('./signup.html'),
+  controller: ['$log', '$location', 'authService', SignupController],
+  controllerAs: 'signupControl'
+};
+
+function SignupController($log, $location, authService) {
+  $log.debug('SignupController');
+
+  authService.getToken()
+  .then( () => {
+    $location.url('/home');
+  });
+
+  this.signup = function(user) {
+    $log.debug('signupControl.signup()');
+
+    authService.signup(user)
+    .then( () => {
+      $location.url('/home');
+    });
+  };
+};
