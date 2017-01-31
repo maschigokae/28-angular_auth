7913de041f2621c7cc1ba8c5a6a1cf0b36447655
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-30 23:32:13 -0800

    build out landing view controller, markup

--- app/view/landing/landing-controller.js
+++ app/view/landing/landing-controller.js
@@ -1,1 +1,12 @@
 'use strict';
+
+require('./_landing.scss');
+
+module.exports = ['$log', '$location', '$rootscope', 'authService', LandingController];
+
+function LandingController($log, $location, authService) {
+  $log.debug('LandingController');
+
+  let url = $location.url();
+  this.showSignup = url === '/join#signup' || url === '/join';
+};
