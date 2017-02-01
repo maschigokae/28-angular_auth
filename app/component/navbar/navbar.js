8d1d87f8dbf5498fcba558bdf2ab9a2038590aa2
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-01 11:24:43 -0800

    build out controller for navbar component

--- app/component/navbar/navbar.js
+++ app/component/navbar/navbar.js
@@ -1,1 +1,43 @@
 'use strict';
+
+require('./_navbar.scss');
+
+module.exports = {
+  template: require('./navbar.html'),
+  controller: ['$log', '$location', '$rootScope', 'authService', NavbarController],
+  controllerAs: 'navbarControl'
+};
+
+function NavbarController($log, $location, $rootScope, authService) {
+  $log.debug('NavbarController');
+
+  this.checkPath = function() {
+    let path = $location.path();
+    if (path === '/join') {
+      this.hideButtons = true;
+    }
+
+    if (path !== '/join') {
+      this.hideButtons = false;
+      authService.getToken()
+      .catch( () => {
+        $location.url('/join#login');
+      });
+    }
+  };
+
+  this.checkPath();
+
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.checkPath();
+  });
+
+  this.logout = function() {
+    $log.log('navbarControl.logout()');
+    this.hideButtons = true;
+    authService.logout()
+    .then( () => {
+      $location.url('/');
+    });
+  };
+};
