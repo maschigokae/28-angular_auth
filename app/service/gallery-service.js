0d5ebfa39c2ec4fddfdd68fcb708efd4779e2026
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-31 20:28:03 -0800

    build out create gallery method

--- app/service/gallery-service.js
+++ app/service/gallery-service.js
@@ -1,3 +1,41 @@
 'use strict';
 
 module.exports = ['$q', '$log', '$http', 'authService', galleryService];
+
+function galleryService($q, $log, $http, authService) {
+  $log.debug('galleryService()');
+
+  let service = {};
+  service.galleries = [];
+
+  service.createGallery = function(gallery) {
+    $log.debug('galleryService.createGallery()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      };
+
+      return $http.post(url, gallery, config);
+    })
+    .then( response => {
+      $log.log('sucess: gallery created');
+
+      let gallery = response.data;
+      service.galleries.unshift(gallery);
+      return gallery;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  return service;
+}
