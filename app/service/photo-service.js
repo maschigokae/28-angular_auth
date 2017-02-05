04aa4c0a3b6a23bf43f0b6e0ec8bd4ae80e30973
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-05 00:19:12 -0800

    build out photo upload method

--- app/service/photo-service.js
+++ app/service/photo-service.js
@@ -3,5 +3,41 @@
 module.exports = ['$q', '$log', '$http', 'Upload', 'authService', photoService];
 
 function photoService($q, $log, $http, Upload, authService) {
-  // TODO: BUILD OUT PHOTO SERVICE
+  $log.debug('photoService()');
+
+  let service = {};
+
+  service.uploadGalleryPhoto = function(galleryData, photoData) {
+    $log.debug('photoService.uploadGalleryPhoto()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryData._id}/pic`;
+      let headers = {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json'
+      };
+
+      return Upload.upload({
+        url,
+        headers,
+        method: 'POST',
+        data: {
+          name: photoData.name,
+          desc: photoData.desc,
+          file: photoData.file
+        }
+      });
+    })
+    .then( response => {
+      galleryData.photos.unshift(response.data);
+      return response.data;
+    })
+    .catch( err => {
+      $log.error('ERROR:', err.message);
+      return $q.reject(err);
+    });
+  };
+
+  return service;
 };
