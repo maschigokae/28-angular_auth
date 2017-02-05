76466c87059112246a7f922120a16951b5edeaa5
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-04 19:08:38 -0800

    build out edit gallery component functionality

--- app/component/gallery/edit-gallery/edit-gallery.js
+++ app/component/gallery/edit-gallery/edit-gallery.js
@@ -1,3 +1,20 @@
 'use strict';
 
 require('./_edit-gallery.scss');
+
+module.exports = {
+  template: require('./edit-gallery.html'),
+  controller: ['$log', 'galleryService', EditGalleryController],
+  controllerAs: 'editGalleryControl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function EditGalleryController($log, galleryService) {
+  $log.debug('EditGalleryController');
+
+  this.updateGallery = function() {
+    galleryService.updateGallery(this.gallery._id, this.gallery);
+  };
+};
