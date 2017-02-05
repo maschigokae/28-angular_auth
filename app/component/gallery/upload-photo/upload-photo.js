c0b3e67fb4b858aa9ff43cffdcce615333d49648
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-05 12:51:39 -0800

    set up upload photo sub-component

--- app/component/gallery/upload-photo/upload-photo.js
+++ app/component/gallery/upload-photo/upload-photo.js
@@ -1,1 +1,27 @@
 'use strict';
+
+require('./_upload-photo.scss');
+
+module.exports = {
+  template: require('./upload-photo.html'),
+  controller: ['$log', 'photoService', UploadPhotoController],
+  controllerAs: 'uploadPhotoControl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function UploadPhotoController($log, photoService) {
+  $log.debug('UploadPhotoController');
+
+  this.photo = {};
+
+  this.uploadPhoto = function() {
+    photoService.uploadGalleryPhoto(this.gallery, this.photo)
+    .then( () => {
+      this.photo.name = null;
+      this.photo.desc = null;
+      this.photo.file = null;
+    });
+  };
+};
