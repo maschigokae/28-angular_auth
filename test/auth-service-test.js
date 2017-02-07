ecd20aec7ce7e8832babae23bad05c88449732fc
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-06 20:21:06 -0800

    add test for authService.getToken method

--- test/auth-service-test.js
+++ test/auth-service-test.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// TODO: BUILD OUT AUTH SERVICE TESTS
+describe('auth service', function() {
+  beforeEach( () => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(($rootScope, authService, $window, $httpBackend) => {
+      this.$window = $window;
+      this.$rootScope = $rootScope;
+      this.authService = authService;
+      this.$httpBackend = $httpBackend;
+    });
+  });
+
+  describe('authService.getToken()', () => {
+    it('should return a token', () => {
+      this.authService.token = null;
+      this.$window.localStorage.setItem('token', 'test token');
+
+      this.authService.getToken()
+      .then( token => {
+        expect(token).toEqual('test token');
+      })
+      .catch( err => {
+        expect(err).toEqual(null);
+      });
+
+      this.$rootScope.$apply();
+    });
+  });
+});
