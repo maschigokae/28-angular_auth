b7e6f83040c8cba63a126ef2ca5dbf305136cf3a
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-20 23:43:37 -0800

    combine thumbnail components, refactor to display photos in carousel

--- app/component/gallery/thumbnail-container/thumbnail-container.js
+++ app/component/gallery/thumbnail-container/thumbnail-container.js
@@ -4,8 +4,20 @@ require('./_thumbnail-container.scss');
 
 module.exports = {
   template: require('./thumbnail-container.html'),
+  controller: ['$log', 'photoService', ThumbnailContainerController],
   controllerAs: 'thumbnailContainerControl',
   bindings: {
-    gallery: '<'
+    gallery: '<',
+    pic: '<'
   }
+};
+
+function ThumbnailContainerController($log, photoService) {
+  $log.debug('ThumbnailContainerController');
+
+  this.deletePhoto = function(gallery, photo) {
+    $log.debug('thumbnailContainerControl.deletePhoto()');
+
+    photoService.deletePhoto(gallery, photo);
+  };
 };
